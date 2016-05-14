f18aa45e3f267e0cacfd1e211066ba816fe77b2a
Jeff Handley <jeff@jeffhandley.com>
2016-05-14 16:18:32 -0700

    Create a validators index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-export { default as maxlength } from './maxlength';
-export { default as maxvalue } from './maxvalue';
-export { default as minlength } from './minlength';
-export { default as minvalue } from './minvalue';
-export { default as required } from './required';
+export * from './validators';
 export { default as ValidationResult } from './ValidationResult';
 
 import * as validation from './validation';
---
+++ src/validators.js
@@ -0,0 +1,7 @@
+export { default as exactlength } from './exactlength';
+export { default as exactvalue } from './exactvalue';
+export { default as maxlength } from './maxlength';
+export { default as maxvalue } from './maxvalue';
+export { default as minlength } from './minlength';
+export { default as minvalue } from './minvalue';
+export { default as required } from './required';
