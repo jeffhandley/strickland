f324bf294bf4c7b735e29696b59054f6ef0bf701
Jeff Handley <jeff@jeffhandley.com>
2017-12-09 10:27:39 -0800

    chore: add more baseline tests; set up test:watch; rename main file

--- test/strickland.spec.js
+++ test/strickland.spec.js
@@ -1,13 +1,60 @@
 import expect from 'expect';
-import strickland, {isValid} from '../src';
+import strickland, {isValid} from '../src/strickland';
 
 describe('strickland', () => {
-    it('returns valid results for null rules and data', () => {
-        const rules = null;
-        const data = null;
+    describe('with rules: null', () => {
+        describe('returns valid results', () => {
+            it('for data: null', () => {
+                const rules = null;
+                const data = null;
 
-        const results = strickland(rules, data);
+                const results = strickland(rules, data);
 
-        expect(isValid(results)).toBe(true);
+                expect(isValid(results)).toBe(true);
+            });
+
+            it('for data: object', () => {
+                const rules = null;
+                const data = {first: 'Jeff'};
+
+                const results = strickland(rules, data);
+
+                expect(isValid(results)).toBe(true);
+            });
+
+            it('for data: string', () => {
+                const rules = null;
+                const data = 'Jeff';
+
+                const results = strickland(rules, data);
+
+                expect(isValid(results)).toBe(true);
+            });
+
+            it('for data: number', () => {
+                const rules = null;
+                const data = 34;
+
+                const results = strickland(rules, data);
+
+                expect(isValid(results)).toBe(true);
+            });
+
+            it('for data: true', () => {
+                const rules = null;
+                const data = true;
+
+                const results = strickland(rules, data);
+                expect(isValid(results)).toBe(true);
+            });
+
+            it('for data: false', () => {
+                const rules = null;
+                const data = false;
+
+                const results = strickland(rules, data);
+                expect(isValid(results)).toBe(true);
+            });
+        });
     });
 });
