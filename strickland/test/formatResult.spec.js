1c36f8f60190e411fb1ad0787ce2ea5baee93f98
Jeff Handley <jeff@jeffhandley.com>
2020-07-06 22:16:59 -0700

    test: add more unit tests for formatResult

--- strickland/test/formatResult.spec.js
+++ strickland/test/formatResult.spec.js
@@ -1,4 +1,5 @@
 import formatResult from '../src/formatResult';
+import {required, minLength, maxLength} from '../src/strickland';
 
 describe('formatResult', () => {
     it('is a function', () => {
@@ -67,7 +68,7 @@ describe('formatResult', () => {
         });
     });
 
-    describe('can be used for objectProps', () => {
+    describe('can be used for the objectProps validator (using the object convention)', () => {
         const validateName = jest.fn(() => false);
 
         const formatter = jest.fn((result) => ({
@@ -101,11 +102,48 @@ describe('formatResult', () => {
             );
         });
 
-        it('applying the formatter', () => {
-            expect(result).toMatchObject({
-                isValid: false,
-                value: expect.objectContaining({name: 'ABC'}),
-                formatted: true
+        describe('applying the formatter', () => {
+            it('to sync results', () => {
+                expect(result).toMatchObject({
+                    isValid: false,
+                    value: expect.objectContaining({name: 'ABC'}),
+                    formatted: true
+                });
+            });
+
+            describe('to async results', () => {
+                const asyncValidate = () => Promise.resolve({
+                    isValid: true,
+                    returnedAsync: true
+                });
+
+                const formatter = (result) => ({
+                    ...result,
+                    formatted: true,
+                    formattedAsync: !!result.objectProps.name.returnedAsync
+                });
+
+                const validate = formatResult(formatter, {
+                    name: asyncValidate
+                });
+
+                const asyncResult = validate({name: 'ABC'});
+
+                it('on the sync result that has async results waiting', () => {
+                    expect(asyncResult).toMatchObject({
+                        isValid: false,
+                        formatted: true,
+                        formattedAsync: false
+                    });
+                });
+
+                it('on the async result after calling validateAsync', () => {
+                    return expect(asyncResult.validateAsync()).resolves.toMatchObject({
+                        isValid: true,
+                        formatted: true,
+                        formattedAsync: true
+                    });
+                });
             });
         });
 
@@ -130,6 +168,227 @@ describe('formatResult', () => {
                 validationErrors: [
                     expect.objectContaining({
                         propName: 'name',
+                        value: 'ABC',
+                        isValid: false
+                    })
+                ]
+            });
+        });
+
+        it('adding a flattened array of validationErrors for nested objects', () => {
+            const withValidationErrors = (result) => ({
+                ...result,
+                validationErrors: Object.keys(result.objectProps)
+                    .filter((propName) => !result.objectProps[propName].isValid && !result.objectProps[propName].validateAsync)
+                    .map((propName) => ({
+                        propName,
+                        ...result.objectProps[propName],
+                    }))
+                    .reduce((accumulator, {objectProps, every, validationErrors, ...propError}) => {
+                        accumulator.push(propError);
+
+                        function addNestedValidationErrors(parentProp, nestedValidationErrors) {
+                            if (nestedValidationErrors) {
+                                nestedValidationErrors.forEach(({objectProps, validationErrors, propName, ...errorResult}) => {
+                                    const nestedPropName = `${parentProp}:${propName}`;
+
+                                    accumulator.push({
+                                        propName: nestedPropName,
+                                        ...errorResult
+                                    });
+
+                                    addNestedValidationErrors(nestedPropName, validationErrors);
+                                });
+                            }
+                        }
+
+                        addNestedValidationErrors(propError.propName, validationErrors);
+
+                        return accumulator;
+                    }, [])
+            });
+
+            const validateObject = formatResult(withValidationErrors, {
+                name: [required(), minLength(1), maxLength(3)],
+                address: [required(), formatResult(withValidationErrors, {
+                    street1: [required(), minLength(1), maxLength(3)],
+                    street2: [minLength(1), maxLength(3)],
+                    city: [required(), minLength(1), maxLength(3)],
+                    state: [required(), minLength(2), maxLength(2)],
+                    postal: [required(), minLength(1), maxLength(3)]
+                })],
+                employer: formatResult(withValidationErrors, {
+                    name: [required(), minLength(1), maxLength(3)],
+                    address: [required(), formatResult(withValidationErrors, {
+                        street1: [required(), minLength(1), maxLength(3)],
+                        street2: [minLength(1), maxLength(3)],
+                        city: [required(), minLength(1), maxLength(3)],
+                        state: [required(), minLength(2), maxLength(2)],
+                        postal: [required(), minLength(1), maxLength(3)]
+                    })]
+                })
+            });
+
+            const data = {
+                name: 'Marty',
+                address: {
+                    city: 'Hill Valley',
+                    state: 'CA'
+                },
+                employer: {
+                    address: {
+                        city: 'Hill Valley',
+                        state: 'CA'
+                    }
+                }
+            };
+
+            const objectResult = validateObject(data);
+
+            expect(objectResult).toMatchObject({
+                isValid: false,
+                validationErrors: [
+                    expect.objectContaining({
+                        propName: 'name',
+                        isValid: false,
+                        maxLength: 3
+                    }),
+                    expect.objectContaining({
+                        propName: 'address',
+                        isValid: false,
+                    }),
+                    expect.objectContaining({
+                        propName: 'address:street1',
+                        isValid: false,
+                        required: true
+                    }),
+                    expect.objectContaining({
+                        propName: 'address:city',
+                        isValid: false,
+                        maxLength: 3
+                    }),
+                    expect.objectContaining({
+                        propName: 'address:postal',
+                        isValid: false,
+                        required: true
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer',
+                        isValid: false
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer:name',
+                        isValid: false,
+                        required: true
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer:address',
+                        isValid: false
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer:address:street1',
+                        isValid: false,
+                        required: true
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer:address:city',
+                        isValid: false,
+                        maxLength: 3
+                    }),
+                    expect.objectContaining({
+                        propName: 'employer:address:postal',
+                        isValid: false,
+                        required: true
+                    })
+                ]
+            });
+        });
+    });
+
+    describe('can be used for the every validator (using the array convention)', () => {
+        const validateValue = jest.fn(() => false);
+
+        const formatter = jest.fn((result) => ({
+            ...result,
+            formatted: true
+        }));
+
+        const validate = formatResult(formatter, [validateValue]);
+
+        const result = validate('ABC');
+
+        it('calling the formatter', () => {
+            expect(formatter).toHaveBeenCalledWith(
+                // result
+                expect.objectContaining({
+                    isValid: false,
+                    value: 'ABC'
+                }),
+
+                // middleware context
+                expect.objectContaining({
+                    value: 'ABC'
+                })
+            );
+        });
+
+        describe('applying the formatter', () => {
+            it('to sync results', () => {
+                expect(result).toMatchObject({
+                    isValid: false,
+                    value: 'ABC',
+                    formatted: true
+                });
+            });
+
+            describe('to async results', () => {
+                const asyncValidate = () => Promise.resolve({
+                    isValid: true,
+                    returnedAsync: true
+                });
+
+                const formatter = (result) => ({
+                    ...result,
+                    formatted: true,
+                    formattedAsync: !!result.returnedAsync
+                });
+
+                const validate = formatResult(formatter, [asyncValidate]);
+
+                const asyncResult = validate('ABC');
+
+                it('on the sync result that has async results waiting', () => {
+                    expect(asyncResult).toMatchObject({
+                        isValid: false,
+                        formatted: true,
+                        formattedAsync: false
+                    });
+                });
+
+                it('on the async result after calling validateAsync', () => {
+                    return expect(asyncResult.validateAsync()).resolves.toMatchObject({
+                        isValid: true,
+                        formatted: true,
+                        formattedAsync: true
+                    });
+                });
+            });
+        });
+
+        it('adding a validationErrors array to the result', () => {
+            const withValidationErrors = (result) => ({
+                ...result,
+                validationErrors: result.every
+                    .filter((everyResult) => !everyResult.isValid && !everyResult.validateAsync)
+            });
+
+            const validateWithValidationErrors = formatResult(withValidationErrors, [validateValue]);
+
+            const resultWithValidationErrors = validateWithValidationErrors('ABC');
+
+            expect(resultWithValidationErrors).toMatchObject({
+                validationErrors: [
+                    expect.objectContaining({
                         value: 'ABC',
                         isValid: false
                     })
